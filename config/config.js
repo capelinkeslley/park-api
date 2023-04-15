54afbe5641695ce92393dc475f1e3b41616f90f9
Keslley Capelin <capelin.cvv@gmail.com>
2023-04-15 14:36:44 -0300

    first commit

---
+++ config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "keslleycapelin",
+  password: "uuwqbr70",
+  database: "park_api_development",
+  define: {
+    timestamps: true,
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt',
+  },
+};
